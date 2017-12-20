c35d4aac7c03b3effc5cecd47c9460054cd3ea0e
Cooper Cummings <coopercummings6@gmail.com>
2017-12-20 10:01:55 -0500

    implemented reverse method using a for loop to iterate through characters in the string in reverse order and append them to a stringbuffer

--- src/main/java/quiz/ReverseString.java
+++ src/main/java/quiz/ReverseString.java
@@ -1,7 +1,18 @@
 package quiz;
 
 public class ReverseString {
+    /*
+    *reverse reverses the order of characters in a string
+    *@param input The string to reverse
+    *@return a string containing the characters of the input in reverse order
+    */
     public String reverse(String input) {
-        return new StringBuffer(input).reverse().toString();
+        StringBuffer output = new StringBuffer();//create stringbuffer to store reversed characters
+
+        for(int i = input.length() - 1; i > -1; i--)    //iterate through input from end to beginning
+        {
+            output.append(input.charAt(i));//append character at cursor to stringbuffer
+        }
+        return output.toString();
     }
 }
